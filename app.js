5f2c9f8148730988a39c15eb22c2553557fe3d9a
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2024-08-24 17:39:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const errorController = require("./controllers/errorController");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'https://dutt.vercel.app', 
+  credentials: true,
+}));
 
 // Routes
 app.use("/api/user", authRouter);
