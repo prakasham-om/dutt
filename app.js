af4570dfaef258033d02fb14bd888b31182d0d0a
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2024-08-25 10:48:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const errorController = require("./controllers/errorController");
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://vercel.com/prakashamoms-projects/dutt', // Your frontend domain
+  origin: 'https://dutt.vercel.app/', // Your frontend domain
   credentials: true // Allow cookies to be sent
 }));
 // Routes
