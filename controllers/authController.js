697b8d97488576cacb18459360dadc570c9aa17a
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2024-09-05 16:09:06 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,7 @@ const assignTokenToCookie = (user, res, statusCode) => {
 
   const cookieOptions = {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
     expires: new Date(
       Date.now() + parseInt(process.env.JWT_EXPIRES_IN) * 24 * 60 * 60 * 1000
     ),
