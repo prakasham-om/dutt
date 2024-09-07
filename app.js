3ebdb8b265289c323923383d67e5d1201cbd532b
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2024-09-07 21:09:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
 
 const app = express();
 const authRouter = require("./routers/authRouter");
@@ -12,49 +11,27 @@ const uploadRouter = require("./routers/uploadRouter");
 const ReqError = require("./utilities/ReqError");
 const errorController = require("./controllers/errorController");
 
-// CORS configuration
-const corsOptions = {
-  origin: 'https://dutt.vercel.app', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // Allow cookies to be sent if needed
-};
-
-app.use(cors(corsOptions)); // Ensure CORS is configured first
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
+app.use(cors());
 
-// Public routes
+// Routes
 app.use("/api/user", authRouter);
 
-
+// Protector
 app.use("/api/*", (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return next(new ReqError(401, "Unauthorized: No token provided"));
-  }
+  if (!req.cookies.userId)
+    return next(new ReqError(400, "You are not logged in"));
 
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return next(new ReqError(401, "Unauthorized: Invalid token"));
-    }
-
-    req.user = decoded;
-    next();
-  });
+  next();
 });
-
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/chatRoom", chatRoomRouter);
 app.use("/api/upload", uploadRouter);
 
-// Middleware to protect routes
-
-
-// Error handling middleware
+// Error handle middleware
 app.use(errorController);
 
 module.exports = app;
