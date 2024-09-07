6bd86305467726e0528b1f783db2c3b1d01bdafe
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2024-09-07 15:25:55 +0000

    Update useFetch.jsx

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,36 +1,43 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { notificationActions } from '../store/notificationSlice';
-import { authActions } from '../store/authSlice'; // Import auth actions
+import { authActions } from '../store/authSlice';
 
-const useFetch = ({ method, url }, successFn, errorFn) => {
+const useFetch = ({ method, url }, successFn, errorFn, isLoginRequest = false) => {
   const [requestState, setRequestState] = useState('idle');
   const dispatch = useDispatch();
-  const token = useSelector((state) => state.authReducer.token); // Access token from authSlice
+  const token = useSelector((state) => state.authReducer.token);
 
   const requestFunction = async (values) => {
     const methodUpper = method.toUpperCase();
-    
+
     const fetchOptions = {
       method: methodUpper,
       headers: {
         'Content-Type': 'application/json',
-        ...(token && { 'Authorization': `Bearer ${token}` }), // Include token if available
-         'Accept': 'application/json'
+        ...(token && !isLoginRequest && { 'Authorization': `Bearer ${token}` }),
+        'Accept': 'application/json',
       },
-      ...(methodUpper !== 'GET' && { body: JSON.stringify(values) }), 
+      ...(methodUpper !== 'GET' && { body: JSON.stringify(values) }),
     };
 
     try {
       setRequestState('loading');
       const response = await fetch(`https://dutt-41dw.onrender.com${url}`, fetchOptions);
 
+      const contentType = response.headers.get('Content-Type');
+      let data;
+
+      if (contentType && contentType.includes('application/json')) {
+        data = await response.json();
+      } else {
+        data = await response.text(); // Handle non-JSON responses if needed
+      }
+
       if (!response.ok) {
-        const data = await response.json();
         throw new Error(data.message || 'An error occurred');
       }
 
-      const data = methodUpper !== 'DELETE' ? await response.json() : null;
       setRequestState('success');
       successFn && successFn(data);
       return data;
