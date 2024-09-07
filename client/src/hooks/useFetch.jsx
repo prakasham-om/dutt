fb9a99fe3ab601ac5c1f65c110fc8f7e3fbd0bf5
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2024-09-07 21:09:55 +0530

    Update useFetch.jsx

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,58 +1,43 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { notificationActions } from '../store/notificationSlice';
-import { authActions } from '../store/authSlice';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { notificationActions } from "../store/notificationSlice";
 
-const useFetch = ({ method, url }, successFn, errorFn, isLoginRequest = false) => {
-  const [requestState, setRequestState] = useState('idle');
+const useFetch = ({ method, url }, successFn, errorFn) => {
+  const [requestState, setRequestState] = useState();
   const dispatch = useDispatch();
-  const token = useSelector((state) => state.authReducer.token);
 
   const requestFunction = async (values) => {
     const methodUpper = method.toUpperCase();
-
-    const fetchOptions = {
-      method: methodUpper,
-      headers: {
-        'Content-Type': 'application/json',
-        ...(token && !isLoginRequest && { 'Authorization': `Bearer ${token}` }),
-        'Accept': 'application/json',
-      },
-      ...(methodUpper !== 'GET' && { body: JSON.stringify(values) }),
-    };
+    const fetchOptions =
+      methodUpper !== "GET"
+        ? {
+            method: methodUpper,
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(values),
+          }
+        : {};
 
     try {
-      setRequestState('loading');
-      const response = await fetch(`https://dutt-41dw.onrender.com${url}`, fetchOptions);
-
-      const contentType = response.headers.get('Content-Type');
+      setRequestState("loading");
+      const response = await fetch(`/api${url}`, fetchOptions);
       let data;
-
-      if (contentType && contentType.includes('application/json')) {
+      if (methodUpper !== "DELETE") {
         data = await response.json();
-      } else {
-        data = await response.text(); // Handle non-JSON responses if needed
       }
-
-      if (!response.ok) {
-        throw new Error(data.message || 'An error occurred');
-      }
-
-      setRequestState('success');
+      if (!response.ok) throw new Error(data.message);
+      setRequestState("success");
       successFn && successFn(data);
       return data;
     } catch (error) {
-      setRequestState('error');
+      setRequestState("error");
       dispatch(
         notificationActions.addNotification({
           message: error.message,
-          type: 'error',
+          type: "error",
         })
       );
-
-      if (error.message.includes('Unauthorized')) {
-        dispatch(authActions.logout()); // Clear token and update state on unauthorized access
-      }
 
       errorFn && errorFn(error);
     }
