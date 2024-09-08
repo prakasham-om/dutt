149659a3f42888e0fa827076e54f907279d91e2f
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2024-09-08 16:18:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
 const app = express();
 const authRouter = require("./routers/authRouter");
 const contactsRouter = require("./routers/contactsRouter");
@@ -11,31 +10,33 @@ const uploadRouter = require("./routers/uploadRouter");
 const ReqError = require("./utilities/ReqError");
 const errorController = require("./controllers/errorController");
 
+// CORS Configuration
+const corsOptions = {
+  origin: 'https://dutt.vercel.app', // Replace with your frontend URL
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true, // Allow cookies to be sent and received
+};
+
+app.use(cors(corsOptions));
+
+// Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({
-  origin: 'https://dutt.vercel.app', // Allow requests from this origin
-  methods: 'GET,POST,PUT,DELETE', // Allowed methods
-  allowedHeaders: 'Content-Type,Authorization' // Allowed headers
-}));app.use(cors());
 
 // Routes
 app.use("/api/user", authRouter);
-
-// Protector
-app.use("/api/*", (req, res, next) => {
-  if (!req.cookies.userId)
-    return next(new ReqError(400, "You are not logged in"));
-
-  next();
-});
-
 app.use("/api/contacts", contactsRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/chatRoom", chatRoomRouter);
 app.use("/api/upload", uploadRouter);
 
-// Error handle middleware
+// Protector Middleware
+app.use("/api/*", (req, res, next) => {
+  if (!req.cookies.userId) return next(new ReqError(400, "You are not logged in"));
+  next();
+});
+
+// Error Handling Middleware
 app.use(errorController);
 
 module.exports = app;
