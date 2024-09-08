b95a5e4a109dfda28dd06708e983346842984216
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2024-09-08 16:32:39 +0530

    Update useFetch.jsx

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -3,36 +3,30 @@ import { useDispatch } from "react-redux";
 import { notificationActions } from "../store/notificationSlice";
 
 const useFetch = ({ method, url }, successFn, errorFn) => {
-  const [requestState, setRequestState] = useState("idle"); // Set initial state to 'idle'
+  const [requestState, setRequestState] = useState();
   const dispatch = useDispatch();
 
   const requestFunction = async (values) => {
     const methodUpper = method.toUpperCase();
-
-    const fetchOptions = {
-      method: methodUpper,
-      headers: {
-        "Content-Type": "application/json",
-        // You can add more headers here if needed
-      },
-      ...(methodUpper !== "GET" && { body: JSON.stringify(values) }), // Add body only if method is not GET
-      credentials: "include", // Include cookies with requests (if needed)
-    };
+    const fetchOptions =
+      methodUpper !== "GET"
+        ? {
+            method: methodUpper,
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(values),
+          }
+        : {};
 
     try {
       setRequestState("loading");
-      const response = await fetch(`/api${url}`, fetchOptions);
-      
-      // Handle non-JSON responses gracefully
-      let data = {};
-      if (response.headers.get("Content-Type")?.includes("application/json")) {
+      const response = await fetch(`https://dutt-41dw.onrender.com/api${url}`, fetchOptions);
+      let data;
+      if (methodUpper !== "DELETE") {
         data = await response.json();
       }
-
-      if (!response.ok) {
-        throw new Error(data.message || "An error occurred");
-      }
-
+      if (!response.ok) throw new Error(data.message);
       setRequestState("success");
       successFn && successFn(data);
       return data;
@@ -40,7 +34,7 @@ const useFetch = ({ method, url }, successFn, errorFn) => {
       setRequestState("error");
       dispatch(
         notificationActions.addNotification({
-          message: error.message || "An error occurred",
+          message: error.message,
           type: "error",
         })
       );
